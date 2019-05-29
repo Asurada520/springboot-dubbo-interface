4e1764bf278d072b2347ed67aa72bb64990e866c
Asurada520 <602423940@qq.com>
2019-05-29 17:36:50 +0800

    dubbo test

---
+++ src/main/java/com/ybzbcq/service/TUserService.java
@@ -0,0 +1,11 @@
+package com.ybzbcq.service;
+
+import com.ybzbcq.model.TUser;
+
+public interface TUserService {
+
+    public String sayHI(String id);
+
+    public TUser getUserById(String id);
+
+}
